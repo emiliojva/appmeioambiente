584453fded5d399ba7be9b8fe17d4558556959bd
JVFeijo <joaovictor_feijo@hotmail.com>
2018-04-17 16:04:24 -0300

    Correção do component de individuo para tronco

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { ExpansionPanelComponent } from './expansion-panel/expansion-panel';
-import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
 	declarations: [ExpansionPanelComponent],
--- src/pages/individuo/individuo.module.ts
+++ src/pages/individuo/individuo.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { IndividuoPage } from './individuo';
-import { ExpansionPanelComponent } from '../../components/expansion-panel/expansion-panel';
 
 
 @NgModule({
   declarations: [
     IndividuoPage,
-    ExpansionPanelComponent
   ],
   imports: [
     IonicPageModule.forChild(IndividuoPage),
