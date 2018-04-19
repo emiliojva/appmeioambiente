5f02e7905089eee63e208d3d09f5420b2c1eb42c
JVFeijo <joaovictor_feijo@hotmail.com>
2018-04-19 17:00:23 -0300

    IndividuoPage progresso

---
+++ src/pages/individuo2/individuo2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Individuo2Page } from './individuo2';
+
+@NgModule({
+  declarations: [
+    Individuo2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Individuo2Page),
+  ],
+})
+export class Individuo2PageModule {}
